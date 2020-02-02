6392a7f0905c5b4bc855432497077495098a001f
Tarun Chopra <tchopra@catalystone.com>
2020-02-02 18:08:31 +0530

    Implemented After advice via xml config

--- src/main/java/com/learning/springaop/aspect/LoggingAspect.java
+++ src/main/java/com/learning/springaop/aspect/LoggingAspect.java
@@ -5,4 +5,8 @@ public class LoggingAspect {
     public void logBeforeCircleGetter(int id, String text) {
         System.out.println("---> Logging before circle getter :: " + id + " | " + text);
     }
+
+    public void logAfterCircleGetter() {
+        System.out.println("---> Logging after circle getter");
+    }
 }
